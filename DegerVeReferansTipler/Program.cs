386f30f7e45102764456a68658f2886edaf8856b
Muhammed Mustafa Bardakci <bardakcimmustafa@gmail.com>
2024-05-17 15:53:50 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int sayi1 = 10;
+        int sayi2 = 30;
+        sayi1 = sayi2;
+        sayi2 = 65;
+        //sayi1 = 30 sayi2 = 65
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,12 @@
+﻿using Matematik;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.Topla(5,4);
+
+        dortIslem.Topla(6,9);
+    }
+}
